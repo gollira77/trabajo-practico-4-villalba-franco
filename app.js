5934d4c8f0dac6fef6bb5de6dcc3a780b46d5a77
Villalba Franco <franquivillalba13@gmail.com>
2025-06-26 11:17:35 -0300

    conexion con la base de datos entre database.js y app.js

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import sequelize from './src/config/database.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Probando API');
+});
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+const init = async () => {
+    try {
+        await sequelize.authenticate(); 
+        console.log('Base de datos conectada');
+
+        await sequelize.sync(); 
+
+        app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error('Error al conectar:', err);
+    }
+};
+
+init();
